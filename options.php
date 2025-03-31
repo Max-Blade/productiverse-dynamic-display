0b1818a42e55d030539801e483881bfe759e3600
Samuel Ramos <sammblackmail@gmail.com>
2025-03-31 18:46:14 -0400

    add(): Add a file for define all constants-> in progress

---
+++ options.php
@@ -0,0 +1,12 @@
+<?php
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'PDD_VERSION', '1.0.0' );
+
+define('PDD_INCLUDES', PDD_DIR_PATH . 'includes/');
+
+define('PDD_LANGUAGE_DIR', PDD_DIR_PATH . 'languages/');
