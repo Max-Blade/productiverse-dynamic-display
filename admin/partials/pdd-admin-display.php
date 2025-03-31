622819fd4c6d850f5bdec36408b8589f7870bbb2
Samuel Ramos <sammblackmail@gmail.com>
2025-03-31 18:54:34 -0400

    refactor(admin): Rename files and class

--- admin/partials/pdd-admin-display.php
+++ admin/partials/pdd-admin-display.php
@@ -4,12 +4,7 @@
  * Provide a admin area view for the plugin
  *
  * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @link       http://example.com
  * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin/partials
  */
 ?>
 
