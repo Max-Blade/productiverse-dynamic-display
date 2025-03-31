cdb319c59237c30f5742e7732e4b50db498f20d9
Samuel Ramos <sammblackmail@gmail.com>
2025-03-31 19:01:39 -0400

    refactor(): Change classnames and const-> done

--- productiverse-dynamic-display.php
+++ productiverse-dynamic-display.php
@@ -1,27 +1,18 @@
 <?php
 
 /**
- * The plugin bootstrap file
+ * @package           Productiverse Dynamic Display
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Productiverse Dynamic Display
+ * Plugin URI:        https://github.com/Max-Blade/productiverse-dynamic-display
+ * Description:       Productiverse Dynamic Display is the ultimate plugin to take your e-commerce store customization to the next level.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Samuel Ramos
+ * Author URI:        https://github.com/Max-Blade/
+ * Author Mail:		  samm.blackmail@gmail.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       productiverse-dynamic-display
  * Domain Path:       /languages
  */
 
@@ -30,29 +21,27 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define('PDD_DIR_PATH', plugin_dir_path( __FILE__ ));
+
+require_once PDD_DIR_PATH . 'options.php';
+
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-pdd-activator.php
  */
 function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+	require_once PDD_INCLUDES . 'class-pdd-activator.php';
+	Productiverse_Dynamic_Display_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-pdd-deactivator.php
  */
 function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+	require_once PDD_INCLUDES . 'class-pdd-deactivator.php';
+	Productiverse_Dynamic_Display_Deactivator::deactivate();
 }
 
 register_activation_hook( __FILE__, 'activate_plugin_name' );
@@ -62,7 +51,7 @@ register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require PDD_INCLUDES . 'class-pdd.php';
 
 /**
  * Begins execution of the plugin.
@@ -75,7 +64,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  */
 function run_plugin_name() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new Productiverse_Dynamic_Display();
 	$plugin->run();
 
 }
