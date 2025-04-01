d1739bb0daaf506bfce336b7fe577fdb5c9fd2a1
Samuel Ramos <sammblackmail@gmail.com>
2025-04-01 11:03:01 -0400

    add(): New constants for paths-> in progress

--- options.php
+++ options.php
@@ -10,3 +10,7 @@ define( 'PDD_VERSION', '1.0.0' );
 define('PDD_INCLUDES', PDD_DIR_PATH . 'includes/');
 
 define('PDD_LANGUAGE_DIR', PDD_DIR_PATH . 'languages/');
+
+define('PDD_ADMIN', PDD_DIR_PATH . 'admin/');
+
+define('PDD_PUBLIC', PDD_DIR_PATH . 'public/');
